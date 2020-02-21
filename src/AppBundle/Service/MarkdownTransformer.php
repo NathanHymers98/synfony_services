c9e75756ea68850a070a3721eea61d7c473e1cd4
Nathan Hymers <nathan.hymers@dgkc2hgcjv3x.wren.wrenkitchens.com>
2020-02-21 15:14:57 +0000

    Second commit

--- src/AppBundle/Service/MarkdownTransformer.php
+++ src/AppBundle/Service/MarkdownTransformer.php
@@ -4,7 +4,35 @@
 namespace AppBundle\Service;
 
 
+use Doctrine\Common\Cache\Cache;
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+
 class MarkdownTransformer
 {
+    private $markdownParser; // Creating the property that will be assigned to the object.
+
+    private $cache;
+
+    public function __construct(MarkdownParserInterface $markdownParser, Cache $cache) // Using a constructor method for Dependency Injection, passing the object I want as the argument
+    {
+        $this->markdownParser = $markdownParser; // Assigning the property to the object
+        $this->cache = $cache;
+    }
+
+    public function parse($str)
+    {
+        $cache = $this->cache;
+        $key = md5($str);
+        if ($cache->contains($key)) {
+            return $cache->fetch($key);
+        }
+
+        sleep(1);
+         $str = $this->markdownParser // Calling the property so that we can use the markdownParser object
+            ->transformMarkdown($str);
+        $cache->save($key, $str);
+
+        return $str;
+    }
 
 }
--- src/AppBundle/Twig/MarkdownExtension.php
+++ src/AppBundle/Twig/MarkdownExtension.php
@@ -4,7 +4,35 @@
 namespace AppBundle\Twig;
 
 
-class MarkdownExtension
+use AppBundle\Service\MarkdownTransformer;
+
+class MarkdownExtension extends \Twig_Extension // Creating a new twig extension so that we can create custom twig filters to use in our twig files
 {
 
+    private $markdownTransformer;
+
+    public function __construct(MarkdownTransformer $markdownTransformer)
+    {
+
+        $this->markdownTransformer = $markdownTransformer;
+    }
+
+    public function getFilters()
+    {
+        return [ // Creating an new twig simple filter object and returning it with my custom filters.
+            new \Twig_SimpleFilter('markdownify', array($this, 'parseMarkdown'), [ // When 'markdownify' is used as a filter in a twig file, call to this array, which is calling to the function parseMarkdown()
+               'is_safe' => ['html']
+            ])
+        ];
+    }
+
+    public function parseMarkdown($str) // When this function is called it returns the string that has the filter on it in upper case.
+    {
+        return $this->markdownTransformer->parse($str);
+    }
+    
+    public function getName()
+    {
+        return 'app_markdown';
+    }
 }
